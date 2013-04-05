378cecb80426d7573d3c8e5220c1e7ead8186576
Thomas Bibb <tbibb1@kec119-12.ycp.edu>
2013-04-05 09:16:19 -0400

    added unit tests project

---
+++ HeatGemProblemDomainClassesTest/src/edu/ycp/cs320/heatgem/shared/GameTest.java
@@ -0,0 +1,20 @@
+package edu.ycp.cs320.heatgem.shared;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class GameTest {
+	private Game game;
+	
+	@Before
+	public void setUp() {
+		game = new Game();
+	}
+	
+	@Test
+	public void testSomething() {
+		assertTrue(true);
+	}
+}
