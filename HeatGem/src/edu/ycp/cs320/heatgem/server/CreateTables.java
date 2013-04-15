257f71131e7dd9df2a7f7395eb728cc71e0af6ba
Kelsie Pike <kpike1@ycp.edu>
2013-04-15 09:45:58 -0400

    created classes that will be used for database

---
+++ HeatGem/src/edu/ycp/cs320/heatgem/server/CreateTables.java
@@ -0,0 +1,14 @@
+package edu.ycp.cs320.heatgem.server;
+
+import java.sql.SQLException;
+
+
+public class CreateTables {
+	
+	public static void main(String[] args) throws SQLException {
+		FakeDatabase db = new FakeDatabase();
+		db.createTables();
+		System.out.println("Successfully created tables");
+	}
+
+}
---
+++ HeatGem/src/edu/ycp/cs320/heatgem/server/ITransaction.java
@@ -0,0 +1,10 @@
+package edu.ycp.cs320.heatgem.server;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public interface ITransaction<E> {
+	
+	public E run(Connection conn) throws SQLException;
+
+}
