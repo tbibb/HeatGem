c8770df89a3ef23c3c117e5579d07ffa33a1e77c
Kelsie Pike <kpike1@ycp.edu>
2013-04-30 17:37:44 -0400

    battle

--- HeatGemProblemDomainClasses/src/edu/ycp/cs320/heatgem/shared/Battle.java
+++ HeatGemProblemDomainClasses/src/edu/ycp/cs320/heatgem/shared/Battle.java
@@ -4,8 +4,8 @@ import java.util.Random;
 
 
 public class Battle {
-	 Player One;
-     Player Two;
+	 private Player One;
+     private Player Two;
 
 
      public Battle(Player playerOne, Player Monster)
@@ -14,15 +14,53 @@ public class Battle {
          Two = Monster;
      }
 
-     public void turn()
+     public void Attack() //Initiate attack phase, and determine what the enemy will do
      {
          Random random = new Random();
+         Random TRandom = new Random();
          int playerDamage = (int)random.nextInt(10);
          int monsterDamage = random.nextInt(10);
-         One.setHealth(One.getHealth() - playerDamage);
-         Two.setHealth(Two.getHealth() - monsterDamage);
+         
+         int MTurn = (int)TRandom.nextInt(2);
+         Two.setHealth(One.getHealth() - playerDamage); //Attack enemy
+         
+         if (MTurn == 1){ //Monster attacks
+        	MonsterAttack();
+         }
+         else
+         {
+        	 MonsterHeal(); //Monster heals
+         }
      }
 
+ 	public void MonsterAttack() {  //Monster attacks
+ 		Random random = new Random();
+ 		int monsterDamage = random.nextInt(10);
+		One.setHealth(One.getHealth() - monsterDamage);
+	}
+
+	public void MonsterHeal() { //Monster heals
+ 		Random random = new Random();
+ 		int monsterHeal = random.nextInt(10);
+		Two.setHealth(Two.getHealth() + monsterHeal);
+	}
+
+	public void heal() { //Initiates instance for player to heal and determines if the enemy will heal as well
+		Random random = new Random();
+		Random TRandom = new Random();
+		int playerHeal = (int)random.nextInt(10);
+		One.setHealth(One.getHealth() + playerHeal);  //Player is healed
+
+		int MTurn = (int)TRandom.nextInt(2);
+		  if (MTurn == 1){ //Monster heals
+	        	MonsterHeal();
+	         }
+	         else
+	         {
+	        	 MonsterAttack(); //Monster attacks
+	         }
+
+	}
      public int battleState()
      {
          if (One.getHealth() <= 0)
@@ -34,11 +72,15 @@ public class Battle {
              return 0; // no one is dead
      }
 
-     public int getHealth(Player player)
+     public int getHealth1()
      {
-         return player.getHealth();
+         return One.getHealth();
      }
 
-    
-     
+     public int getHealth2()
+     {
+         return Two.getHealth();
+     }
+
+
  }
