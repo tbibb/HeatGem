57f1beac92711509949c26bb5622aeed0a385321
Kelsie Pike <kpike1@ycp.edu>
2013-05-09 15:22:02 -0400

    made skeletal unit tests for Tom to implement

--- HeatGemProblemDomainClassesTest/src/edu/ycp/cs320/heatgem/shared/ScoreTest.java
+++ HeatGemProblemDomainClassesTest/src/edu/ycp/cs320/heatgem/shared/ScoreTest.java
@@ -4,16 +4,19 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class ScoreTest {
+
 	
-	 private int playerScore;
+	
 	@Before 
 	public void setUp() {
-	playerScore = 2345;
+	
+		
+		
 	}
 	
 	@Test
 	public void testGetScore(){
-		//assertEquals(2345.0, playerScore.getScore(), DELTA); 
+		 
 		
 	}
 	
